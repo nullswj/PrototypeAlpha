ecc0e113cd01db91c277f392b2c2a22afa5d092c
nullswj <46858769+nullswj@users.noreply.github.com>
2019-04-25 01:59:36 +0800

    201904250159

--- app/src/main/java/com/swj/prototypealpha/swj/ProjectListActivity.java
+++ app/src/main/java/com/swj/prototypealpha/swj/ProjectListActivity.java
@@ -10,17 +10,15 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
 
-import com.swj.prototypealpha.swj.util.searchView.SearchView;
+import com.swj.prototypealpha.activity.NoticeListActivity;
+import com.swj.prototypealpha.activity.NoticeResultListActivity;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
@@ -29,79 +27,21 @@ import com.chad.library.adapter.base.listener.OnItemDragListener;
 import com.chad.library.adapter.base.listener.OnItemSwipeListener;
 import com.swj.prototypealpha.R;
 import com.swj.prototypealpha.swj.util.RecyclerViewHelper.ItemDragAdapter;
-import com.swj.prototypealpha.swj.util.searchView.Bean;
-import com.swj.prototypealpha.swj.util.searchView.SearchAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProjectListActivity extends AppCompatActivity implements SearchView.SearchViewListener{
+public class ProjectListActivity extends AppCompatActivity{
 
     private static final String TAG = ProjectListActivity.class.getSimpleName();
 
     private Toolbar tlb_pri_list;
 
     /**
-     * 搜索结果列表view
-     */
-    private ListView lvResults;
-
-    /**
      * 搜索view
      */
     private SearchView searchView;
 
-
-    /**
-     * 热搜框列表adapter
-     */
-    private ArrayAdapter<String> hintAdapter;
-
-    /**
-     * 自动补全列表adapter
-     */
-    private ArrayAdapter<String> autoCompleteAdapter;
-
-    /**
-     * 搜索结果列表adapter
-     */
-    private SearchAdapter resultAdapter;
-
-    private List<Bean> dbData;
-
-    /**
-     * 热搜版数据
-     */
-    private List<String> hintData;
-
-    /**
-     * 搜索过程中自动补全数据
-     */
-    private List<String> autoCompleteData;
-
-    /**
-     * 搜索结果的数据
-     */
-    private List<Bean> resultData;
-
-    /**
-     * 默认提示框显示项的个数
-     */
-    private static int DEFAULT_HINT_SIZE = 4;
-
-    /**
-     * 提示框显示项的个数
-     */
-    private static int hintSize = DEFAULT_HINT_SIZE;
-
-    /**
-     * 设置提示框显示项的个数
-     *
-     * @param hintSize 提示框显示个数
-     */
-    public static void setHintSize(int hintSize) {
-        ProjectListActivity.hintSize = hintSize;
-    }
 
 
     private void setToolbar()
@@ -114,107 +54,36 @@ public class ProjectListActivity extends AppCompatActivity implements SearchView
 
     }
 
-    /**
-     * 初始化数据
-     */
-    private void initData() {
-        //从数据库获取数据
-        getDbData();
-        //初始化热搜版数据
-        getHintData();
-        //初始化自动补全数据
-        getAutoCompleteData(null);
-        //初始化搜索结果数据
-        getResultData(null);
-    }
 
-    /**
-     * 获取db 数据
-     */
-    private void getDbData() {
-        int size = 4;
-        dbData = new ArrayList<>(size);
 
-        dbData.add(new Bean(R.drawable.icon, "项目", "万家丽路BRT中途停靠站建设项目", " "));
-        dbData.add(new Bean(R.drawable.icon, "项目", "橘子洲大桥提质改造工程", " "));
-        dbData.add(new Bean(R.drawable.icon, "项目", "湘府路快速化改造建设项目第一标段主体工程", " "));
-        dbData.add(new Bean(R.drawable.icon, "项目", "湘府路快速化改造道路工程第三标段", " "));
 
-    }
 
-    /**
-     * 获取热搜版data 和adapter
-     */
-    private void getHintData() {
-        hintData = new ArrayList<>(hintSize);
-        for (int i = 1; i <= hintSize; i++) {
-            hintData.add("热搜版" + i + "：项目");
-        }
-        hintAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, hintData);
-    }
 
-    /**
-     * 获取自动补全data 和adapter
-     */
-    private void getAutoCompleteData(String text) {
-        if (autoCompleteData == null) {
-            //初始化
-            autoCompleteData = new ArrayList<>(hintSize);
-        } else {
-            // 根据text 获取auto data
-            autoCompleteData.clear();
-            for (int i = 0, count = 0; i < dbData.size() && count < hintSize; i++) {
-                if (dbData.get(i).getTitle().contains(text.trim())) {
-                    autoCompleteData.add(dbData.get(i).getTitle());
-                    count++;
-                }
-            }
-        }
-        if (autoCompleteAdapter == null) {
-            autoCompleteAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, autoCompleteData);
-        } else {
-            autoCompleteAdapter.notifyDataSetChanged();
-        }
-    }
 
-    /**
-     * 获取搜索结果data和adapter
-     */
-    private void getResultData(String text) {
-        if (resultData == null) {
-            // 初始化
-            resultData = new ArrayList<>();
-        } else {
-            resultData.clear();
-            for (int i = 0; i < dbData.size(); i++) {
-                if (dbData.get(i).getTitle().contains(text.trim())) {
-                    resultData.add(dbData.get(i));
-                }
-            }
-        }
-        if (resultAdapter == null) {
-            resultAdapter = new SearchAdapter(this, resultData, R.layout.item_bean_list);
-        } else {
-            resultAdapter.notifyDataSetChanged();
-        }
-    }
 
     /**
      * 初始化视图
      */
     private void initViews() {
-        lvResults = (ListView) findViewById(R.id.proj_info_lv_search_results);
         searchView = (SearchView) findViewById(R.id.search_view_proj_info);
-        //设置监听
-        searchView.setSearchViewListener(this);
-        //设置adapter
-        searchView.setTipsHintAdapter(hintAdapter);
-        searchView.setAutoCompleteAdapter(autoCompleteAdapter);
-
-        lvResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        searchView.setQueryHint("查询通知");
+        searchView.setIconified(true);
+        searchView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                Toast.makeText(ProjectListActivity.this, position + "", Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                searchView.setIconified(false);
+            }
+        });
+        searchView.setOnQueryTextListener(new android.support.v7.widget.SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
             }
         });
     }
@@ -226,7 +95,6 @@ public class ProjectListActivity extends AppCompatActivity implements SearchView
 
         setToolbar();
 
-        initData();
         initViews();
 
         RecyclerView mRecyclerView = findViewById(R.id.rv_project_list);
@@ -332,28 +200,6 @@ public class ProjectListActivity extends AppCompatActivity implements SearchView
         return data;
     }
 
-    @Override
-    public void onRefreshAutoComplete(String text) {
-        //更新数据
-        getAutoCompleteData(text);
-    }
-
-    @Override
-    public void onSearch(String text) {
-        //更新result数据
-        getResultData(text);
-        lvResults.setVisibility(View.VISIBLE);
-        //第一次获取结果 还未配置适配器
-        if (lvResults.getAdapter() == null) {
-            //获取搜索数据 设置适配器
-            lvResults.setAdapter(resultAdapter);
-        } else {
-            //更新搜索数据
-            resultAdapter.notifyDataSetChanged();
-        }
-        Toast.makeText(this, "完成搜索", Toast.LENGTH_SHORT).show();
-
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
